ae61d24e3c70220c82cc079fdfae67f91032ccfb
naman <naman.khalkuria@juspay.in>
2020-02-01 19:15:40 +0530

    react added

---
+++ precache-manifest.cfef60d812682ce598cd97c695744ed1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a750682a1fac1720593a1fd1f45724",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c5f3d31f3bbdbc27b81",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7e23973ee1b0c48d8403",
+    "url": "/static/js/2.e4469489.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e4469489.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c5f3d31f3bbdbc27b81",
+    "url": "/static/js/main.2484d37c.chunk.js"
+  },
+  {
+    "revision": "c2ad5c23fb2789f3f3dd",
+    "url": "/static/js/runtime-main.903fb561.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
